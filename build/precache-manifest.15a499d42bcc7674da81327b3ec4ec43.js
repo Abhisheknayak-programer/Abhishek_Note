467f9e9414c2abe469f0acfe3f7831a3521e360a
Abhishek nayak <sahilservice200@gmail.com>
2020-08-14 18:03:51 +0530

    version 2 Abhishek Note APP

---
+++ build/precache-manifest.15a499d42bcc7674da81327b3ec4ec43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223886a483d0a6fb66823b566a20d37e",
+    "url": "/Abhishek_Note/index.html"
+  },
+  {
+    "revision": "6bd6f8fe0a64dbf49114",
+    "url": "/Abhishek_Note/static/css/main.f222588c.chunk.css"
+  },
+  {
+    "revision": "78529b3d330936ec7950",
+    "url": "/Abhishek_Note/static/js/2.6f74773d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Abhishek_Note/static/js/2.6f74773d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bd6f8fe0a64dbf49114",
+    "url": "/Abhishek_Note/static/js/main.73e8cc9d.chunk.js"
+  },
+  {
+    "revision": "13136b9fe0c2f0e9f08e",
+    "url": "/Abhishek_Note/static/js/runtime-main.ece15c18.js"
+  },
+  {
+    "revision": "f1a029e5428ddc477115b84b27b93de0",
+    "url": "/Abhishek_Note/static/media/1.f1a029e5.jpeg"
+  }
+]);
